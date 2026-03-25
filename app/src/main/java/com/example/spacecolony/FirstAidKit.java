de2647181ed066b40e4e49c4b3b2812b131b0c0f
Pablo Garcia <discoverscovers@gmail.com>
2026-03-25 17:45:57 +0200

    Commit

--- app/src/main/java/com/example/spacecolony/FirstAidKit.java
+++ app/src/main/java/com/example/spacecolony/FirstAidKit.java
@@ -1,12 +1,10 @@
 package com.example.spacecolony;
 
 public class FirstAidKit extends Item {
-    private int hpRestore;
     public FirstAidKit () {
-        super(30);
-        this.hpRestore = 10;
+        super(60);
     }
     public void used (CrewMember cm) {
-        cm.heal(this.hpRestore);
+        cm.heal(1000000);
     }
 }
